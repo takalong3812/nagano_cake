4a5cc2b5339138b9272f4bb9fd75594cb2b21813
EC2 Default User <ec2-user@ip-172-31-6-235.ap-northeast-1.compute.internal>
2022-12-17 12:23:59 +0000

    商品編集画面作成開始

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -8,13 +8,33 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-        redirect_to admin_item_path(@item.id)
+        redirect_to admin_item_path(@item.id), notice: "新商品登録完了！"
     else
      frash.now[:alert] = "もう一度入力し直してください"
      render "index"
     end
  
-  end  
+  end
+  
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item.id)
+    
+  end
+  
+  
+  def index
+    @items = Item.all
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+
+  end
+  
+  
+
   
   
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,11 @@ class Item < ApplicationRecord
   
   validates :name, presence: true
   validates :price, presence: true
+  
+  def add_tax_price
+   (self.price * 1.10).round
+  end
+  
+  
+  
 end
