cebd614ead39393e198bd28d103f1462b1654c51
EC2 Default User <ec2-user@ip-172-31-6-235.ap-northeast-1.compute.internal>
2023-01-09 14:53:33 +0000

    画像OK

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -12,10 +12,6 @@ class Public::ItemsController < ApplicationController
      @cart_item= CartItem.new
    end
    
-   private
-   
-   def item_params
-    params.require(:item).permit(:image)
-   end
+ 
     
 end
